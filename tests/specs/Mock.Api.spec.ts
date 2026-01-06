1768b58bde3b3c82a5bbe779b74f41a44eab4785
Manish Solanki <mk394@MKSolanki>
2026-01-06 10:56:43 +0530

    github action demo test

--- tests/specs/Mock.Api.spec.ts
+++ tests/specs/Mock.Api.spec.ts
@@ -22,7 +22,7 @@ test.describe('Mock API Tests', () => {
     });
     
     // JSON Server returns 201 for POST
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(201);
 
     const json = await response.json();
     console.log(json);
