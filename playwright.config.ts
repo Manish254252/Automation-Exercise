36f20bfdeb137dc2a040c63e47da762776e08f32
Manish Solanki <mk394@MKSolanki>
2026-01-06 12:19:06 +0530

    github action demo test

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,10 +25,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   // workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-   reporter: [
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['json', { outputFile: 'playwright-report/result.json' }],
-    ['list']
+  reporter: [
+    ['allure-playwright'],
+    ['html', { open: 'on-failure' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -36,7 +35,6 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless:true,
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
